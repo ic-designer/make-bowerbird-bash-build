6eb92ef9d4c25a3f860acaf958ebecd4b838f0c3
Jeff Fredenburg <jfreden@fnal.gov>
2024-06-05 11:47:13 -0500

    better organize constants across Makefile and private.mk

--- make/private.mk
+++ make/private.mk
@@ -1,14 +1,15 @@
 # Config
 .SUFFIXES:
+MAKEFLAGS += --jobs
 MAKEFLAGS += --no-builtin-rules
 MAKEFLAGS += --no-builtin-variables
 MAKEFLAGS += --no-print-directory
-MAKEFLAGS += --jobs
+MAKEFLAGS += --warn-undefined-variables
 
 # Constants
-NAME ?= $(error ERROR: Undefined variable NAME)
-VERSION ?= $(error ERROR: Undefined variable VERSION)
-WORKDIR_ROOT ?= $(error ERROR: Undefined variable WORKDIR_ROOT)
+override NAME := bowerbird-bash-builder
+VERSION := $(shell git describe --always --dirty --broken 2> /dev/null)
+WORKDIR_ROOT := $(CURDIR)/.make
 override WORKDIR_DEPS = $(WORKDIR_ROOT)/deps
 override WORKDIR_TEST = $(WORKDIR_ROOT)/test/$(NAME)/$(VERSION)
 
